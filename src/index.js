7f28f5506a07b95e2f1be8dfb3334fd55673de30
mrkavalsky <70824301+mrkavalsky@users.noreply.github.com>
2020-10-25 20:56:18 +0300

    feat:  implement function that reverse digits of this number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  if(n < 0) n = -n;
+  let str = '';
+  n = n + '';
+  for(let i = 0; i < n.length; i++){
+      str = n[i] + str;
+  };
+  n = +str;
+  return n;
 }
